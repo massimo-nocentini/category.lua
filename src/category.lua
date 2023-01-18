e470790d713ff69cbf5f1f2ef2a7748190dc74aa
Massimo Nocentini <massimo.nocentini@gmail.com>
2023-01-18 17:35:59 +0100

    Some functors and applicatives.

---
+++ src/category.lua
@@ -0,0 +1,121 @@
+
+local C = { }
+
+local just = {}
+just.__index = just
+
+function C.just (v)
+
+    local j = {value = v}
+    setmetatable (j, just)
+
+    return j
+end
+
+function just.__eq (v, w)
+    if getmetatable (w) == just then return v.value == w.value
+    else return false end
+end
+
+function just.__tostring (cat)
+    return string.format ('just (%s)', tostring (cat.value))
+end
+
+function just.fmap (cat, f)
+    return C.just ( f (cat.value) )
+end
+
+function just.pure (cat, v)
+    return C.just (v)
+end
+
+function just.applicative (cat, cat_f)
+    return cat:fmap (cat_f.value)
+end
+
+
+local list = {}
+list.__index = list
+
+function C.list (v)
+
+    local j = {value = v}
+    setmetatable (j, list)
+
+    return j
+end
+
+function list.__eq (v, w)
+    if getmetatable (w) == list then 
+        local lv, lw = v.value, w.value
+        if #lv == #lw then
+            local eq = true
+            for i = 1, #lv do eq = eq and lv[i] == lw[i] end
+            return eq
+        else return false end
+    else return false end
+end
+
+function list.__tostring (cat)
+    local s = {}
+    for i, v in ipairs (cat.value) do s[i] = tostring (v) end
+    return string.format ('list (%s)', table.concat (s, ', '))
+end
+
+function list.fmap (cat, f)
+    local l = {}
+    for i, v in ipairs (cat.value) do l[i] = f (v) end
+    return C.list (l)
+end
+
+function list.pure (cat, v)
+    return C.list (v)
+end
+
+function list.applicative (cat, cat_f)
+    local l = {}
+    for i, f in pairs (cat_f.value) do l[i] = cat:fmap (f) end
+    return C.list (l)
+end
+
+local fun = {}
+fun.__index = fun
+
+function C.fun (v)
+
+    local j = {value = v}
+    setmetatable (j, fun)
+
+    return j
+end
+
+function fun.__eq (v, w)
+    if getmetatable (w) == fun then return v.value == w.value 
+    else return false end
+end
+
+function fun.__tostring (cat)
+    return string.format ('fun (%s)', tostring (cat.value))
+end
+
+function fun.__call (cat, ...)
+    return cat.value (...)
+end
+
+function fun.fmap (cat, f)
+    return C.fun (function (...) return f (cat.value (...)) end)
+end
+
+function C.fmap (f)
+    return function (cat) 
+        return cat:fmap (f)
+    end
+end
+
+function C.applicative (cat_f)
+    return function (cat) 
+        return cat:applicative (cat_f)
+    end
+end
+
+return C
---
+++ test/test.lua
@@ -0,0 +1,117 @@
+
+local lu = require 'luaunit'
+local C = require 'category'
+local lambda = require 'operator'
+
+local inc = C.fmap (function (v) return v + 1 end)
+
+local function replicate (n)
+    return function (v)
+        local t = {}
+        for i = 1, n do t[i] = v end
+        return C.list (t)
+    end
+end
+
+Test_just = {}
+
+function Test_just:test_eq ()
+    local j = C.just (3)
+    lu.assertEquals (j, C.just (4 - 1))
+end
+
+function Test_just:test_tostring ()
+    local j = C.just (4 - 1)
+    lu.assertEquals (tostring(j), 'just (3)')
+end
+
+function Test_just:test_fmap ()
+    local j = C.just (3)
+    lu.assertEquals (inc (j) , C.just (3 + 1))
+end
+
+function Test_just:test_replicate ()
+    local three = 3
+    local j = C.just (three)
+    local repl = C.fmap (replicate (4))
+    lu.assertEquals ( repl (j) , C.just (C.list {three, three, three, three}))
+end
+
+function Test_just:test_applicative ()
+    local three = 3
+    local j = C.just (function (v) return v + 1 end)
+    local w = j:pure (three)
+    lu.assertEquals (C.applicative (j) (w), C.just (three + 1))
+end
+
+--------------------------------------------------------------------------------
+
+Test_list = {}
+
+function Test_list:test_tostring ()
+    local j = C.list {2, 3, 4, 5}
+    lu.assertEquals (tostring(j), 'list (2, 3, 4, 5)')
+end
+
+function Test_list:test_eq ()
+    local i = C.list {2, 3, 4, 5}
+    local j = C.list {2, 3, 4, 6}
+    local k = C.list {2, 3, 4, 5, 6}
+    local l = C.list {2, 3, 4, 6 - 1}
+    lu.assertEquals (i, l)
+    lu.assertNotEquals (i, j)
+    lu.assertNotEquals (j, k)
+    lu.assertNotEquals (k, i)
+end
+
+function Test_list:test_fmap ()
+    local j = C.list {2, 3, 4, 5}
+    lu.assertEquals (inc (j) , C.just {3, 4, 5, 6})
+end
+
+function Test_list:test_replicate ()
+    local j = C.list {2, 3, 4, 5}
+    local repl = C.fmap (replicate (3))
+    lu.assertEquals ( repl (j) , C.list {
+        C.list {2, 2, 2}, 
+        C.list {3, 3, 3}, 
+        C.list {4, 4, 4}, 
+        C.list {5, 5, 5},
+    })
+end
+
+function Test_list:test_applicative ()
+    local three = 3
+    local elements = C.list {2, 3, 4, 5}
+    local functions = C.applicative (
+        C.list {
+            function (v) return v * 0 end, 
+            function (v) return v + 100 end, 
+            function (v) return v ^ 2 end
+        }
+    )
+    lu.assertEquals (functions (elements), C.list {
+        C.list {0, 0, 0, 0}, 
+        C.list {102, 103, 104, 105}, 
+        C.list {4.0, 9.0, 16.0, 25.0},
+    })
+end
+
+--------------------------------------------------------------------------------
+
+Test_fun = {}
+
+function Test_fun:test_fmap ()
+    local double = C.fun (function (v) return v * 2 end)
+    lu.assertEquals (inc (double) (3), 3 * 2 + 1 )
+end
+
+function Test_fun:test_replicate ()
+    local double = C.fun (function (v) return v * 2 end)
+    local repl = C.fmap (replicate (3))
+    lu.assertEquals ( repl (double) (3) , C.list {6, 6, 6})
+end
+
+--------------------------------------------------------------------------------
+
+os.exit( lu.LuaUnit.run() )
